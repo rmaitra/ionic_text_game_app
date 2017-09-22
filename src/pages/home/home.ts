24f9b6130358bd80b0698ff7fcf768566c573e50
rmaitra <raj.d.maitra@gmail.com>
2017-09-21 23:12:37 -0700

    updated.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-    public items: any = ["test", "asfd"]
-    public objects: any = [
-        {
-            name:"master.ai",
-            text:"Welcome."
-        }
-    ]
+    public operation:string;
+    public result: any={};
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private http:Http) {
 
     }
 
-    public parse(text:string){
-        console.log(text)
-        let obj:any = {
-            name:"user",
-            text:text
-        }
-        this.objects.push(obj);
-        
+    public async operate(text:string){
+        let response = await this.request_newton(text).toPromise()
+        this.result = response;
+    }
+
+    public request_newton(text:string):any{
+        return this.http.get("https://newton.now.sh/" + this.operation + "/" + text)
+            .map(function(response: Response){
+                return response.json()
+            })
     }
 }
